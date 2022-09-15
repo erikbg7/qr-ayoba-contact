d26653a84075977c983ace6170e5b043da5a5132
erikbg7 <erik.bg7@gmail.com>
2022-09-15 14:03:19 +0200

    Improve vCard QR design

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,15 @@ const profile = {
 
 const getVCardData = (profile: Profile) => {
   const { name: vCardFormattedName, phone: vCardPhone } = profile;
-  const vCardName = profile.name.split(' ').reverse().join(';');
+  const vCardName = profile.name.split(' ').reverse().join(';').concat(';');
 
-  return 'BEGIN:VCARD\nVERSION:2.1\nN:Blancaddd;Erik;;\nFN:Erik Blanca\nTEL;HOME;VOICE:+34646070475\nEND:VCARD';
+  return `BEGIN:VCARD\nVERSION:2.1\nN:${vCardName}\nFN:${vCardFormattedName}\nTEL;HOME;VOICE:${vCardPhone}\nEND:VCARD`;
 };
 
 const App = () => {
   return (
     <main className="flex flex-col justify-center items-center h-[100vh]">
-      <section className="w-fit h-fit p-8 text-center bg-blue-400/90 rounded-2xl">
-        <h1 className="text-gray-50 text-4xl font-semibold"> {profile.name} </h1>
-        <h2 className="text-gray-300 text-2xl mb-6"> {profile.phone} </h2>
+      <section className="w-fit h-fit p-3 text-center bg-blue-400/90 rounded-[1rem]">
         <QRCodeSVG
           value={getVCardData(profile)}
           size={256}
@@ -34,13 +32,15 @@ const App = () => {
           includeMargin={true}
           imageSettings={{
             src: 'https://developer.ayoba.me/_next/image?url=%2Fassets%2FLogo%2FAyobaFinalIcon.png&w=128&q=75',
-            x: undefined,
-            y: undefined,
             height: 64,
             width: 64,
             excavate: true,
           }}
         />
+        <footer className="my-4">
+          <h1 className="text-gray-50 text-4xl font-semibold"> {profile.name} </h1>
+          <h2 className="text-gray-300 text-2xl"> {profile.phone} </h2>
+        </footer>
       </section>
       <p className="text-lg text-gray-400 mt-8 mb-20">Scan the QR code to add the contact</p>
     </main>
