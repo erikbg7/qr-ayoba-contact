0f6df8cd4b391803aa382ab1117338e03822c127
erikbg7 <erik.bg7@gmail.com>
2022-09-16 12:27:09 +0200

    Improve skeleton animation

--- src/components/QrCodeVCardSkeleton.tsx
+++ src/components/QrCodeVCardSkeleton.tsx
@@ -5,11 +5,11 @@ const QrCodeVCardSkeleton = () => {
     <section className="w-fit h-fit p-3 text-center bg-blue-400/90 rounded-2xl">
       <div className="shadow animate-pulse rounded-xl bg-gray-700/40 text-transparent w-[256px] h-[256px]" />
       <footer className="my-4">
-        <h1 className="text-4xl text-transparent">
-          -<span className="shadow animate-pulse rounded-xl bg-gray-700/40"></span>-
+        <h1 className="text-4xl text-transparent flex items-center justify-center">
+          -<span className="shadow animate-pulse rounded-xl bg-gray-700/40 h-8 w-[70%]"></span>-
         </h1>
-        <h2 className="text-2xl shadow animate-pulse rounded-xl bg-gray-700/40 text-transparent">
-          Phone
+        <h2 className="text-2xl text-transparent flex items-center justify-center">
+          -<span className="shadow animate-pulse rounded-xl bg-gray-700/40 h-6 w-[80%]"></span>-
         </h2>
       </footer>
     </section>
