09662b90aa26d8d7fcd820b21113548a90715c5f
erikbg7 <erik.bg7@gmail.com>
2022-10-17 18:05:19 +0200

    Remove logs and payments PoC

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,6 @@ import React from 'react';
 import { QrCodeVCard } from './components/QrCodeVCard';
 import { QrCodeVCardSkeleton } from './components/QrCodeVCardSkeleton';
 
-const handlePaymentStart = () => {
-  const ayobaApi = (window as any)?.Ayoba;
-  ayobaApi.startPayment('MoMo', 100, 'XAF', 'Test payment');
-  // (window as any)?.startPayment?.({
-  //   method: 'MoMo',
-  //   amount: 199,
-  //   currency: 'XAF',
-  //   description: 'Test Payment',
-  // });
-};
-
 const App = () => {
   const [profile, setProfile] = React.useState({ nickname: '', phone: '' });
   const isLoading = !profile.nickname || !profile.phone;
@@ -32,10 +21,6 @@ const App = () => {
       {!isLoading && <QrCodeVCard nickname={profile.nickname} phone={profile.phone} />}
       {isLoading && <QrCodeVCardSkeleton />}
       <p className="text-lg text-gray-400 mt-8 mb-20">Scan the QR code to add the contact</p>
-
-      <div>
-        <button onClick={handlePaymentStart}>Start Payment 2</button>
-      </div>
     </main>
   );
 };
